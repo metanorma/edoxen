1469275f4641acd1d9a8e52b6e5bd199ce5af1f6
Marcin Wagner <git@exine.eu>
2023-03-03 15:51:04 +0100

    implement basic types

--- lib/edoxen/subject_body.rb
+++ lib/edoxen/subject_body.rb
@@ -3,3 +3,12 @@
 #   address
 #   etc.
 # }
+require "shale"
+require "shale/type/value"
+
+module Edoxen
+  class SubjectBody < Shale::Mapper
+    attribute :name, Shale::Type::String
+    attribute :address, Shale::Type::String
+  end
+end
